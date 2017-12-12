399cf0830c96374a7153ef479e4694463d14f188
yinguobing <yinguobing@gmail.com>
2017-12-12 17:21:20 +0800

    add code to fit the faceboxes

--- pts_tools.py
+++ pts_tools.py
@@ -7,9 +7,10 @@ import os
 import numpy as np
 
 import cv2
+import detect_face as fd
 
-IMG_DIR = "/home/robin/Documents/landmark/dataset/ibug"
-IMG_FORMAT = ".jpg"
+IMG_DIR = "/home/robin/Documents/landmark/dataset/lfpw/testset"
+IMG_FORMAT = ".png"
 
 
 def read_points(file_name=None):
@@ -52,6 +53,32 @@ def preview(point_file):
     img = cv2.imread(os.path.join(
         IMG_DIR, point_file.split(".")[-2] + IMG_FORMAT))
 
+    # Get the face bounding boxes.
+    conf, faceboxes = fd.get_facebox(img, threshold=0.9)
+    fd.draw_result(img, conf, faceboxes)
+
+    # Get the square boxs contains face.
+    square_boxes = fd.get_square_box(faceboxes)
+
+    # Remove false positive boxes.
+    min_x = min([point[0] for point in raw_points])
+    max_x = max([point[0] for point in raw_points])
+    min_y = min([point[1] for point in raw_points])
+    max_y = max([point[1] for point in raw_points])
+    for idx, box in enumerate(square_boxes):
+        if box[0] > min_x or box[1] > min_y or box[2] < max_x or box[3] < max_y:
+            del square_boxes[idx]
+    fd.draw_box(img, square_boxes)
+
+    # Check if fitting required.
+    rows = img.shape[0]
+    cols = img.shape[1]
+    for box in square_boxes:
+        if box[0] < 0 or box[1] < 0 or box[2] > cols or box[3] > rows:
+            fited_box = fd.fit_box(img, raw_points, square_boxes[0])
+            if fited_box is not None:
+                fd.draw_box(img, [fited_box], box_color=(255, 0, 0))
+
     # Draw the landmark points.
     draw_landmark_point(img, raw_points)
 
@@ -61,7 +88,7 @@ def preview(point_file):
     if height > max_height:
         img = cv2.resize(img, (max_height, int(width * max_height / height)))
     cv2.imshow("preview", img)
-    cv2.waitKey(100)
+    cv2.waitKey()
 
 
 def main():

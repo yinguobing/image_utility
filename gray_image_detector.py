3ee9d93b97cd36ec362ec05b261a00639b283dd7
yinguobing <yinguobing@gmail.com>
2018-08-22 17:20:27 +0800

    add file to detect single channel images

---
+++ gray_image_detector.py
@@ -0,0 +1,32 @@
+import cv2
+from file_list_generator import ListGenerator
+from tqdm import tqdm
+
+
+def main():
+    lg = ListGenerator()
+    files_to_check = lg.generate_list(
+        '/data/dataset/public/coco', ['jpg'])
+    print("Total files: {}".format(len(files_to_check)))
+
+    gray_img_list = []
+    num_checked = 0
+    with tqdm(total=len(files_to_check)) as pbar:
+        for each_file in files_to_check:
+            num_checked += 1
+            if num_checked % 100 == 0:
+                pbar.update(100)
+                
+            img = cv2.imread(each_file, cv2.IMREAD_ANYCOLOR)
+
+            if len(img.shape) != 3:
+                gray_img_list.append(each_file)
+                cv2.imshow("gray", img)
+                if cv2.waitKey(100) == 27:
+                    break
+
+    print("Total gray images: {}".format(len(gray_img_list)))
+
+
+if __name__ == '__main__':
+    main()

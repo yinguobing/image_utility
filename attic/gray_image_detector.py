2ce064a7d0bad49637c7535c5942ff28469a6547
yinguobing <yinguobing@gmail.com>
2019-07-17 10:30:22 +0800

    add arguments parser

--- attic/gray_image_detector.py
+++ attic/gray_image_detector.py
@@ -1,18 +1,25 @@
+from argparse import ArgumentParser
+
 import cv2
-from file_list_generator import ListGenerator
 from tqdm import tqdm
+
+from file_list_generator import ListGenerator
+
+parser = ArgumentParser()
+parser.add_argument('dir', type=str, help='target path')
+args = parser.parse_args()
 
 
 def main():
     lg = ListGenerator()
-    files_to_check = lg.generate_list(
-        '/data/dataset/public/coco', ['jpg'])
+    files_to_check = lg.generate_list(args.dir, ['jpg'])
     print("Total files: {}".format(len(files_to_check)))
 
     gray_img_list = []
     num_checked = 0
-    for each_file in tqdm(files_to_check):
+    for each_file in tqdm(files_to_check[54145:54146]):
         img = cv2.imread(each_file, cv2.IMREAD_ANYCOLOR)
+        print(each_file)
 
         # Preview gray images.
         if len(img.shape) != 3:
